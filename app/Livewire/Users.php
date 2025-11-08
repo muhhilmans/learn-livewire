49b967a3d292a99395d0cfe802ce7789907db80f
Muh Hilman Sholehudin <muhhilmansholehudin@gmail.com>
2025-11-08 23:05:28 +0700

    feat: Adding Example Actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -7,6 +7,15 @@ use Livewire\Component;
 
 class Users extends Component
 {
+    public function createUser()
+    {
+        User::create([
+            'name' => 'New User' . rand(1, 1000),
+            'email' => 'newuser' . rand(1, 1000) . '@example.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.users', [
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,5 +1,16 @@
-<div>
+<div class="p-6">
     <h1 class="text-3xl font-bold text-center">{{ $title }}</h1>
 
     <p>{{ $users->count() }} users found.</p>
+
+    <button wire:click="createUser" type="button" class="bg-blue-500 hover:bg-blue-800 text-white font-medium px-4 py-2 cursor-pointer rounded-lg my-2">Create User</button>
+
+    <hr class="border-2 border-gray-200 my-2">
+    <ul class="list-disc pl-5">
+        @foreach($users as $user)
+            <li>
+                <span class="font-semibold">{{ $user->name }}</span> - {{ $user->email }}
+            </li>
+        @endforeach
+    </ul>
 </div>
